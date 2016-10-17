6fb0d24649dcb34541faa36241940f6781216555
Adrian Mejia <admejiar@cisco.com>
2016-10-17 13:09:04 -0400

    ng generate component todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/todo/todo.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { TodoComponent } from './todo.component';
+
+describe('Component: Todo', () => {
+  it('should create an instance', () => {
+    let component = new TodoComponent();
+    expect(component).toBeTruthy();
+  });
+});
