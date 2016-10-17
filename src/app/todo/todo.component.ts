9b13f6b2f9096fabe15eef5f282be03242a32bc1
Adrian Mejia <admejiar@cisco.com>
2016-10-17 16:18:45 -0400

    READ: Get all tasks

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TodoService } from './todo.service';
+
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
+  providers: [TodoService]
 })
 export class TodoComponent implements OnInit {
+  private todos;
 
-  constructor() { }
+  constructor(private todoService: TodoService) { }
 
-  ngOnInit() {
+  getTodos(){
+    return this.todoService.get().then(todos => {
+      this.todos = todos;
+    });
   }
 
+  ngOnInit() {
+    this.getTodos();
+  }
 }
--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
 
+let todos = [
+  { title: 'Install Angular CLI', isDone: true },
+  { title: 'Style app', isDone: true },
+  { title: 'Finish service functionality', isDone: false },
+  { title: 'Setup API', isDone: false },
+];
+
 @Injectable()
 export class TodoService {
 
   constructor() { }
+
+  get(){
+    return new Promise(resolve => resolve(todos));
+  }
 
 }
