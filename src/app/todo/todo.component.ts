33e2b3e268789de449ecbf521bc3501e649f8d07
Adrian Mejia <admejiar@cisco.com>
2018-08-07 15:37:00 -0400

    fix issue with private variables
    Property is private and only accessible within class

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -10,10 +10,10 @@ import { TodoService } from './todo.service';
   providers: [TodoService]
 })
 export class TodoComponent implements OnInit {
-  private todos;
-  private activeTasks;
-  private newTodo;
-  private path;
+  public todos;
+  public newTodo;
+  public activeTasks;
+  public path;
 
   constructor(private todoService: TodoService, private route: ActivatedRoute) { }
 
