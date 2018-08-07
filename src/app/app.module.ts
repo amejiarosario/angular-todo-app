3a38b4d8e0a77facd40e7769c575ea47bbaa7dbf
Adrian Mejia <admejiar@cisco.com>
2018-08-06 22:06:00 -0400

    Routing and Navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
 
+const routes: Routes = [
+  { path: ':status', component: TodoComponent },
+  { path: '**', redirectTo: '/all' }
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +17,8 @@ import { TodoComponent } from './todo/todo.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
