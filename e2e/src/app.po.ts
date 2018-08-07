dfbd19c29561574c415d9607c7d14ada8fd8c20c
Adrian Mejia <admejiar@cisco.com>
2018-08-06 20:21:54 -0400

    angular/cli@6.1.2: ng new

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,6 +1,6 @@
-import { browser, element, by } from 'protractor';
+import { browser, by, element } from 'protractor';
 
-export class Todos2Page {
+export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { TodoComponent } from './todo/todo.component';
-
-const routes: Routes = [
-  { path: ':status', component: TodoComponent },
-  { path: '**', redirectTo: '/all' }
-];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TodoComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    RouterModule.forRoot(routes)
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
