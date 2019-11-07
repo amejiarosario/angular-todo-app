f1270ef51db43d6640df4b327677cd8e069fbcdf
Adrian Mejia <admejiar@cisco.com>
2019-11-07 16:04:27 -0500

    UPDATE: on double click

--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -25,4 +25,12 @@ export class TodoService {
     });
   }
 
+  put(changed) {
+    return new Promise(resolve => {
+      const index = TODOS.findIndex(todo => todo === changed);
+      TODOS[index].title = changed.title;
+      resolve(changed);
+    });
+  }
+
 }
