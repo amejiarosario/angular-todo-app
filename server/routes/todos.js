6178e503548b1d4a690ae663cf3508982a85bba8
Adrian Mejia <me@adrianmejia.com>
2020-03-06 18:07:40 -0500

    create todo

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -3,22 +3,21 @@ const router = express.Router();
 
 const Todo = require('../schemas/todo');
 
-/* POST /api/todos */
-router.post('/', async (req, res) => {
-  try {
-    console.log({body: req.body})
-    const todo = await Todo.create(req.body);
-    res.json(todo);
-  } catch (error) {
-    res.json(500, { error });
-  }
-});
-
 /* GET /api/todos */
 router.get('/', async (req, res) => {
   try {
     const list = await Todo.find();
     res.json(list);
+  } catch (error) {
+    res.json(500, { error });
+  }
+});
+
+/* POST /api/todos */
+router.post('/', async (req, res) => {
+  try {
+    const todo = await Todo.create(req.body);
+    res.json(todo);
   } catch (error) {
     res.json(500, { error });
   }
