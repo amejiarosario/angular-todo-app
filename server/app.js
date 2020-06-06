15f6e2524c6b97787c4269ee4f2a3257ab99fe3b
Adrian Mejia <adriansky@gmail.com>
2020-06-06 19:42:12 -0400

    add db string to connect to mongo

--- server/app.js
+++ server/app.js
@@ -3,12 +3,28 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var todosRouter = require('./routes/todos');
 
 var app = express();
+
+// connect to db
+const user = process.env.MONGO_USER;
+const mongoPort = process.env.MONGO_PORT || 27017;
+const mongoHost = process.env.MONGO_HOST || 'localhost';
+const auth = user ? `${user}:${process.env.MONGO_PASS}@` : '';
+const DB_STRING = `mongodb://${auth}${mongoHost}:${mongoPort}/todos`;
+
+console.log(`Running node ${process.version}...`);
+console.log(`Connecting to DB... ${DB_STRING}`);
+
+const config = { useNewUrlParser: true, useUnifiedTopology: true};
+mongoose.connect(DB_STRING, config)
+  .then(() => console.log(`Connected!`))
+  .catch(console.error);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
