f4f22816ba105748385670c2bddf0919c41ea041
Adrian Mejia <adriansky@gmail.com>
2020-06-06 19:20:54 -0400

    Adding all all the API routes to modify data in DB

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -1,17 +1,55 @@
 const express = require('express');
 const router = express.Router();
 
-const TODOS = [
-  { title: 'Create API to get this list', isDone: true },
-  { title: 'Connect API with Angular', isDone: true },
-  { title: 'Connect server with mongo', isDone: false },
-  { title: 'Publish app', isDone: false },
-];
+const Todo = require('../models/todo');
 
 /* GET /api/todos */
 router.get('/', async (req, res) => {
   try {
-    res.json(TODOS);
+    const list = await Todo.find(req.query);
+    res.json(list);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+/* POST /api/todos */
+router.post('/', async (req, res) => {
+  try {
+    const todo = await Todo.create(req.body);
+    res.json(todo);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+/* PUT /api/todos/:id */
+router.put('/:id', async (req, res) => {
+  try {
+    const options = { new: true };
+    const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, options);
+    res.json(todo);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+/* DELETE /api/todos/:id */
+router.delete('/:id', async (req, res) => {
+  try {
+    const todo = await Todo.findByIdAndDelete(req.params.id);
+    res.json(todo);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+// Beaware: it can delete all data from db if body is empty
+/* DELETE /api/todos */
+router.delete('/', async (req, res) => {
+  try {
+    const todo = await Todo.deleteMany(req.body);
+    res.json(todo);
   } catch (error) {
     res.status(500).json({ error });
   }
