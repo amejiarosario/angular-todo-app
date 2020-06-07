a93291ced8bb5c13f2bc9cbe9b338e81ff1177d5
Adrian Mejia <adriansky@gmail.com>
2020-06-06 20:44:45 -0400

    Angular service using HTTP client

--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -1,59 +1,45 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-const API = '/api/todos';
+export interface ITodo {
+  _id?: string;
+  title: string;
+  isDone: boolean;
+  notes: string;
+  update_at: string;
+  editing ?: boolean;
+}
 
-let TODOS = [
-  { title: 'Install Angular CLI', isDone: true },
-  { title: 'Style app', isDone: true },
-  { title: 'Finish service functionality', isDone: false },
-  { title: 'Setup API', isDone: false },
-];
+const API = '/api/todos';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
-
   constructor(private http: HttpClient) { }
 
   get(params = {}) {
     return this.http.get(API, { params });
   }
 
-  add(data) {
-    return new Promise(resolve => {
-      TODOS.push(data);
-      resolve(data);
-    });
+  add(data: ITodo) {
+    return this.http.post(API, data);
   }
 
-  put(changed) {
-    return new Promise(resolve => {
-      const index = TODOS.findIndex(todo => todo === changed);
-      TODOS[index].title = changed.title;
-      resolve(changed);
-    });
+  put(changed: ITodo) {
+    return this.http.put(`${API}/${changed._id}`, changed);
   }
 
-  delete(selected) {
-    return new Promise(resolve => {
-      const index = TODOS.findIndex(todo => todo === selected);
-      TODOS.splice(index, 1);
-      resolve(true);
-    });
-  }
-
-  deleteCompleted() {
-    return new Promise(resolve => {
-      TODOS = TODOS.filter(todo => !todo.isDone);
-      resolve(TODOS);
-    });
-  }
-
-  toggle(selected) {
+  toggle(selected: ITodo) {
     selected.isDone = !selected.isDone;
-    return Promise.resolve();
+    return this.put(selected);
   }
 
+  delete(selected: ITodo) {
+    return this.http.delete(`${API}/${selected._id}`);
+  }
+
+  deleteCompleted(body = { isDone: true }) {
+    return this.http.request('delete', `${API}`, { body });
+  }
 }
