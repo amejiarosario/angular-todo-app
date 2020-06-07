e33d540f96f6b18f6bc8417772bd45ded7cdc71b
Adrian Mejia <adriansky@gmail.com>
2020-06-06 20:45:08 -0400

    Angular TodoComponet updates

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { TodoService } from './todo.service';
+import { TodoService, ITodo } from './todo.service';
 
 @Component({
   selector: 'app-todo',
@@ -10,66 +10,57 @@ import { TodoService } from './todo.service';
   providers: [TodoService]
 })
 export class TodoComponent implements OnInit {
-  public todos;
-  public activeTasks;
-  public newTodo;
-  public path;
+  public todos: ITodo[];
+  public activeTasks: number;
+  public newTodo: string;
+  public path: string;
   public mapToQuery = {
     all: {},
     active: { isDone: false },
     completed: { isDone: true },
   };
-
   constructor(private todoService: TodoService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-      this.path = params['status'];
+      this.path = params.status;
       this.getTodos(this.path);
     });
   }
 
-  addTodo(){
-    this.todoService.add({ title: this.newTodo, isDone: false }).then(() => {
-      return this.getTodos();
-    }).then(() => {
-      this.newTodo = ''; // clear input form value
-    });
+  addTodo() {
+    this.todoService
+      .add({ title: this.newTodo, isDone: false } as unknown as ITodo)
+      .subscribe(() => {
+        this.getTodos();
+        this.newTodo = ''; // clear input form value
+      });
   }
 
-  getTodos(route = 'all'){
+  getTodos(route = 'all') {
     const query = this.mapToQuery[route];
     return this.todoService
       .get(query)
-      .subscribe(todos => {
+      .subscribe((todos: ITodo[]) => {
         this.todos = todos;
         this.activeTasks = this.todos.filter(todo => !todo.isDone).length;
       });
   }
 
-  updateTodo(todo, newValue) {
+  updateTodo(todo: ITodo, newValue: string) {
     todo.title = newValue;
-    return this.todoService.put(todo).then(() => {
-      todo.editing = false;
-      return this.getTodos();
-    });
+    return this.todoService.put(todo).subscribe(() => this.getTodos());
   }
 
-  destroyTodo(todo) {
-    this.todoService.delete(todo).then(() => {
-      return this.getTodos();
-    });
+  destroyTodo(todo: ITodo) {
+    this.todoService.delete(todo).subscribe(() => this.getTodos());
+  }
+
+  toggleTodo(todo: ITodo) {
+    this.todoService.toggle(todo).subscribe(() => this.getTodos());
   }
 
   clearCompleted() {
-    this.todoService.deleteCompleted().then(() => {
-      return this.getTodos();
-    });
-  }
-
-  toggleTodo(todo) {
-    this.todoService.toggle(todo).then(() => {
-      return this.getTodos();
-    });
+    this.todoService.deleteCompleted().subscribe(() => this.getTodos());
   }
 }
